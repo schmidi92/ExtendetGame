6d91b568b569ebde731fbe9b2ae1f5c37b963762
Markus <Schmidi@dhcp-10-179-8-203.dynamic.eduroam.mwn.de>
2018-04-11 14:40:19 +0200

    .

--- src/edu/hm/cs/laufferschmidt/dialog/FileDialog.java
+++ src/edu/hm/cs/laufferschmidt/dialog/FileDialog.java
@@ -2,9 +2,6 @@ package edu.hm.cs.laufferschmidt.dialog;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
